517b3280cf592c4e539a95775114fae426d62355
Giovanny Gutiérrez <giovanny.gutierrez@merlinjobs.com>
2020-08-18 16:00:35 -0500

    feat: :wrench: Change the include for IPluginManger

--- Source/FFMPEGMedia/Private/FFMPEGMediaModule.cpp
+++ Source/FFMPEGMedia/Private/FFMPEGMediaModule.cpp
@@ -7,7 +7,7 @@
 
 #include "IFFMPEGMediaModule.h"
 #include "Core.h"
-#include "IPluginManager.h"
+#include "Interfaces/IPluginManager.h"
 
 #include "IMediaModule.h"
 #include "Modules/ModuleInterface.h"
